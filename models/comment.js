9900080e8048663978e1fc6a6657fbbdb3316c2a
junghoonK <rhdtl78@gmail.com>
2017-12-09 15:35:33 +0900

    이벤트에 코멘트 기능 추가
    이벤트에 대해 사용자가 코멘트를 달수 있게 됨

--- models/comment.js
+++ models/comment.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 var schema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: 'User' },
-  Event: { type: Schema.Types.ObjectId, ref: 'Event' },
+  event: { type: Schema.Types.ObjectId, ref: 'Event' },
   content: {type: String, trim: true, required: true},
   numLikes: {type: Number, default: 0},
   createdAt: {type: Date, default: Date.now}
